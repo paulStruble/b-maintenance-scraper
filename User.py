c6b46e3c0fdeeda8fcc0ebb6a73e3c89017067cd
pjstruble1@gmail.com <pjstruble1@gmail.com>
2023-09-04 02:44:14 -0700

    created User.py + get methods

---
+++ User.py
@@ -0,0 +1,13 @@
+# a user with attached login info
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def get_username(self):
+        return self.username
+
+    def get_password(self):
+        return self.password
+    
