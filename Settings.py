f1692fed23329edd44205b377c5a32750eaa28d4
pjstruble1@gmail.com <pjstruble1@gmail.com>
2023-09-04 06:42:54 -0700

    created Settings

---
+++ Settings.py
@@ -0,0 +1,7 @@
+# configurable settings for various program features
+#TODO: add print and logging options for all functions (maybe add Log class)
+
+class Settings:
+    def __init__(self):
+        generate_logs = True
+        enable_console_output = True
