dff2e0b62071e9faae1a21a6451668d136515030
pjstruble1@gmail.com <pjstruble1@gmail.com>
2023-09-04 08:12:24 -0700

    select_request_button() (not working)

--- WebAutomation.py
+++ WebAutomation.py
@@ -3,9 +3,11 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
 
 class WebAutomation:
-    # logs in to CalNet using credentials from prompt
+    # logs in to CalNet with the given [driver] as [user]
     @staticmethod
     def login_calnet(driver, user):
         url = "https://auth.berkeley.edu/cas/login?service=https://maintenance.housing.berkeley.edu/cas2/login.aspx"
@@ -16,9 +18,16 @@ class WebAutomation:
         driver.find_element(By.ID, "password").send_keys(user.password)
 
         # click signin button
-        driver.find_element(By.ID, "submit").click()
+        driver.find_element(By. ID, "submit").click()
 
         # wait for DUO Mobile confirmation
         print('---CONFIRM LOGIN ON DUO MOBILE---')
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "dont-trust-browser-button")))
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "dont-trust-browser-button"))).click()
         print('---LOGIN CONFIRMED---')
+
+    # selects "Work Request" from the maintenance tracking dropdown menu
+    @staticmethod
+    def select_request_button(driver):
+        dropdown = driver.find_element(By.CSS_SELECTOR, "[name='Search']")
+        #dropdown_select = Select(driver.find_element(By.XPATH, "//select[@name='Search']"))  # TODO
+        #dropdown_select.select_by_value("WR")
