831e7c5ac9924e2f44b675013d537e81ac34d208
pjstruble1@gmail.com <pjstruble1@gmail.com>
2023-09-04 05:54:12 -0700

    constructor fix, login_calnet()

--- Scraper.py
+++ Scraper.py
@@ -1,17 +1,24 @@
+# tool to scrape work order requests from maintenance website
+
 #from bs4 import beatifulsoup4
 #import requests
 from selenium import webdriver
+
+from WebAutomation import *
 from User import *
 
 class Scraper:
     def __init__(self):
         self.current_user = None
-        self.driver = webdriver.chrome
+        self.driver = webdriver.Chrome()
 
     # prompts user for calnet login
     def login_calnet(self):
         print('CALNET LOGIN:')
         self.current_user = User.login_prompt()
 
-    def scrape_request(self):
+        WebAutomation.login_calnet(self.driver, self.current_user)
+
+    # TODO: scrapes a work order request with the number [request_number]
+    def scrape_request(self, request_number):
         return None
