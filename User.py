c9ac9d6b8b700a4afd648104acedb623ec7a724d
pjstruble1@gmail.com <pjstruble1@gmail.com>
2023-09-04 03:59:11 -0700

    added login_prompt()

--- User.py
+++ User.py
@@ -1,13 +1,17 @@
 # a user with attached login info
 
+from getpass import getpass
+
+
 class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-    def get_username(self):
-        return self.username
-
-    def get_password(self):
-        return self.password
-    
+    # prompts the user for login credentials and returns a new User object with the same credentials
+    @staticmethod
+    def login_prompt():
+        print('Username: ')
+        new_username = input()
+        new_password = getpass()
+        return User(new_username, new_password)
