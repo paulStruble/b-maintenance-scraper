b57825dacd2804d3df482e77c12403a7ca937c3c
pjstruble1@gmail.com <pjstruble1@gmail.com>
2023-09-17 06:30:35 -0700

    to_list(), to_csv()

--- WORequest.py
+++ WORequest.py
@@ -18,3 +18,24 @@ class WORequest:
         self.work_order_num = None
         self.area_description = None
         self.requested_action = None
+
+    # returns a list of all datapoints in string format
+    def to_list(self) -> list:
+        return [self.id,
+                self.room,
+                self.status,
+                self.building,
+                self.tag,
+                self.accept_date,
+                self.reject_date,
+                self.reject_reason,
+                self.location,
+                self.item_description,
+                self.work_order_num,
+                self.area_description,
+                self.requested_action]
+
+    # returns a comma-separated string of all datapoints
+    def to_csv(self) -> str:
+        string_list = [str(datapoint) for datapoint in self.to_list()]
+        return ','.join(string_list)
