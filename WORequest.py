cb416f132b07c0acef7ed24b80a6f2c81629eeaa
pjstruble1@gmail.com <pjstruble1@gmail.com>
2023-09-17 06:10:54 -0700

    WORequest constructor

--- WORequest.py
+++ WORequest.py
@@ -1,3 +1,20 @@
 # A work order request
 
-class Request:
+from typing import Callable
+
+
+class WORequest:
+    def __init__(self, number):
+        self.id = number
+        self.room = None
+        self.status = None
+        self.building = None
+        self.tag = None
+        self.accept_date = None
+        self.reject_date = None
+        self.reject_reason = None
+        self.location = None
+        self.item_description = None
+        self.work_order_num = None
+        self.area_description = None
+        self.requested_action = None
