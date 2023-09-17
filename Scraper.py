169aefbb291ea41521492a6d1c3a3566a317766e
pjstruble1@gmail.com <pjstruble1@gmail.com>
2023-09-17 07:22:43 -0700

    scrape_request return type specification

--- Scraper.py
+++ Scraper.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 
 from WebAutomation import *
 from User import *
+import WORequest
 
 class Scraper:
     def __init__(self):
@@ -22,6 +23,7 @@ class Scraper:
         WebAutomation.login_calnet(self.driver, self.current_user)
 
     # TODO: scrapes a work order request with the number [request_number]
-    def scrape_request(self, request_number):
+    # returns a WORequest
+    def scrape_request(self, request_number: int) -> WORequest:
         WebAutomation.select_request_button(self.driver)
         WebAutomation.search_request(self.driver, request_number)
