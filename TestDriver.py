889cde4fa7a8e2c25ac80d5e6ecafd0d9e18b1d5
Paul <pjstruble1@gmail.com>
2023-09-18 21:21:45 -0700

    iterative scraping

--- TestDriver.py
+++ TestDriver.py
@@ -4,5 +4,9 @@ from Scraper import *
 
 scraper = Scraper()
 scraper.login_calnet()
-scraper.scrape_request(220149)
+for request_number in range(100000, 200001, 10000):
+    try:
+        print(scraper.scrape_request(request_number).to_csv())
+    except:
+        print(f"failed to print request #{request_number}")
 input()
