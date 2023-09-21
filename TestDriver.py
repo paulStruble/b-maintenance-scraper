801a15928f5dc5bf988b8e05c98ace2b11957bc6
Paul <pjstruble1@gmail.com>
2023-09-20 22:15:02 -0700

    new manual testing

--- TestDriver.py
+++ TestDriver.py
@@ -4,9 +4,17 @@ from Scraper import *
 
 scraper = Scraper()
 scraper.login_calnet()
-for request_number in range(100000, 200001, 10000):
+for request_number in range(100000, 200001, 5000):
     try:
         print(scraper.scrape_request(request_number).to_csv())
     except:
         print(f"failed to print request #{request_number}")
 input()
+
+"""current_request = scraper.scrape_request(1)
+if current_request:
+    print(current_request.to_csv())
+
+print("execution complete")"""
+
+print(scraper.scrape_request(0).to_csv())
