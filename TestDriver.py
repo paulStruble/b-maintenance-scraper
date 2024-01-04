764b79eac1e16501b8e0e350c78b1fc392b42f0a
pjstruble1@gmail.com <pjstruble1@gmail.com>
2024-01-03 20:02:23 -0800

    log test

--- TestDriver.py
+++ TestDriver.py
@@ -20,15 +20,21 @@ from Scraper import *
 #
 # print(scraper.scrape_request(0).to_csv())
 
-scraper = Scraper()
-database = RequestDB()
+# scraper = Scraper()
+# database = RequestDB()
+#
+# scraper.login_calnet()
+# for request_id in range(200000, 400001, 202):
+#     try:
+#         database.add_request(request_id, scraper)  # TODO: log
+#     except:
+#         print(f"Error inserting with Request ID: {request_id}")
+#
+# database.close()
+# input()
 
-scraper.login_calnet()
-for request_id in range(200000, 400001, 201):
-    try:
-        database.add_request(scraper.scrape_request(request_id)) # TODO: log
-    except:
-        print(f"Error inserting with Request ID: {request_id}")
+from Log import *
 
-database.close()
-input()
+my_log = Log()
+my_log.add("this is a call to Log.add!")
+my_log.add_quiet("this is a call to Log.add_quiet!")
