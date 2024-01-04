38a2bdd9433d2ef9ffa7c1bb63506bdef4702a01
pjstruble1@gmail.com <pjstruble1@gmail.com>
2024-01-03 20:02:10 -0800

    new settings (not implemented)

--- Settings.py
+++ Settings.py
@@ -1,7 +1,14 @@
 # configurable settings for various program features
-#TODO: add print and logging options for all functions (maybe add Log class)
+# TODO: add print and logging options for all functions (maybe add Log class)
+# TODO: add parallel processing
+# TODO: comments + standardize comments
+# TODO: implement log path
 
 class Settings:
     def __init__(self):
+        log_path = '../'
         generate_logs = True
-        enable_console_output = True
+        console_output = True
+        headless = True
+        parallel = False
+        cores = 1
