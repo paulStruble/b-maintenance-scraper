2b1a7c4bc14f0cd70e83f3553a4ace9ad3f4e5fc
pjstruble1@gmail.com <pjstruble1@gmail.com>
2024-01-08 20:31:17 -0800

    parallel processing update

--- Driver.py
+++ Driver.py
@@ -13,15 +13,16 @@ class Driver:
         self.database = RequestDB(self.log, user)
 
     def main_menu(self):
-        options = [1, 2]
-        exit = 2
+        options = [1, 2, 3]
+        exit_value = 3
         choice = None
-        while choice != exit:
+        while choice != exit_value:
             print("""
             ------------ Main Menu ------------
             Options:
             1. Scrape a range of work order requests and write to your database
-            2. Exit
+            2. Settings
+            3. Exit
             -----------------------------------
             """)
             choice = int(input())
@@ -32,15 +33,25 @@ class Driver:
                 case 1:
                     self.scrape_range_prompt()
                 case 2:
+                    self.settings_menu()
+                case 3:
                     return None
 
     def scrape_range_prompt(self):
         start = int(input("start id: "))
-        end = int(input("end id: "))
+        stop = int(input("stop id: "))
+        processes = int(input("processes: "))
 
-        print(f"Initializing process: scrape and write requests from ids [{start}] to [{end}]")
-        self.database.add_request_range(start, end, self.scraper)
-        print(f"Finished scraping requests from ids [{start}] to [{end}]")
+        print(f"Initializing process: scrape and write requests from ids [{start}] to [{stop}] on [{processes}] processes")
+        if processes > 1:
+            self.database.add_request_range_parallel(start, stop, process_count=processes)
+        else:
+            self.database.add_request_range(start, stop, scraper=self.scraper)
+        print(f"Finished scraping requests from ids [{start}] to [{stop}]")
+
+    def settings_menu(self):
+        print("Settings not yet implemented - please edit config file manually")
+        # TODO: read/print config, ability to edit config from cli
 
     def run(self):
         self.main_menu()
