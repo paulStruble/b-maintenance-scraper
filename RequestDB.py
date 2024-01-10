c91280c3ddbbc588113286a3b9d82fd7802e239e
pjstruble1@gmail.com <pjstruble1@gmail.com>
2024-01-09 23:13:08 -0800

    new parallel processing

--- RequestDB.py
+++ RequestDB.py
@@ -9,24 +9,21 @@ from Log import *
 import multiprocessing
 import traceback
 from User import *
+from typing import Iterable
 
 
 class RequestDB:
-    def __init__(self, log: Log, calnet_user: User, driver_cookies: list[dict] = None, host="localhost", dbname="postgres", user="postgres", password="postgres", port=5432):
+    def __init__(self, log: Log, calnet_user: User, process_id: int = 0, headless=True, host="localhost",
+                 dbname="postgres", user="postgres", password="postgres", port=5432):
+        self.scraper = Scraper(user=calnet_user, process_id=process_id, headless=headless)
         self.log = log
-        self.calnet_user = calnet_user
-        self.driver_cookies = driver_cookies
-
-        self.host = host
-        self.dbname = dbname
-        self.user = user
-        self.password = password
-        self.port = port
+        self.db_name = dbname
+        self.db_args = (host, dbname, user, password, port)
 
         self.all_columns = ["id", "room", "status", "building", "tag", "accept_date", "reject_date", "reject_reason",
                             "location", "item_description", "work_order_num", "area_description", "requested_action"]
 
-        self.connection = psycopg2.connect(host=self.host, dbname=self.dbname, user=self.user, password=self.password, port=self.port)
+        self.connection = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
         self.cursor = self.connection.cursor()
 
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS requests (
@@ -49,17 +46,18 @@ class RequestDB:
         self.connection.commit()
 
     # scrape and insert a work request into the database
-    def add_request(self, request_id: int, scraper: Scraper, cursor):
+    def add_request(self, request_id: int):
+        # noinspection PyBroadException
         try:
             # skip this request if an entry with the same id already exists
             select_query = f"SELECT * FROM requests WHERE id = {request_id}"
-            cursor.execute(select_query)
-            if cursor.fetchone():
+            self.cursor.execute(select_query)
+            if self.cursor.fetchone():
                 self.log.add(f"entry with id [{request_id}] already exists ... skipping this insert request")
                 return None
 
             # scrape request
-            request = scraper.scrape_request(request_id)
+            request = self.scraper.scrape_request(request_id)
 
             # filter out all null values and their corresponding columns
             columns = [c for c in self.all_columns if getattr(request, c)]
@@ -68,54 +66,22 @@ class RequestDB:
             values = ', '.join([f"'{v}'" for v in values])
             insert_query = f"INSERT INTO requests ({columns}) VALUES ({values});"
 
-            cursor.execute(insert_query)
+            self.cursor.execute(insert_query)
             self.connection.commit()
-            self.log.add(f"successfully inserted request [{request_id}] to database [{self.dbname}]")
+            self.log.add(f"successfully inserted request [{request_id}] to database [{self.db_name}]")
         except Exception as e:
             self.log.add(f"failed to insert request [{request_id}]")
             self.log.add_quiet(f"{traceback.format_exc()}\n")
 
-    # TODO: run parallel directly from add_request_range using boolean parallel argument
-    # scrape and insert a range of work requests into the database (sequential)
-    def add_request_range(self, start: int, stop: int, scraper: Scraper = None, cursor = None):
-        if not cursor:
-            cursor = self.cursor
+    # scraper and insert work requests for an iterable
+    def add_requests(self, request_ids: Iterable[int]):
+        for request_id in request_ids:
+            self.add_request(request_id)
+
+    # scrape and insert a range of work requests into the database
+    def add_request_range(self, start: int, stop: int):
         for request_id in range(start, stop):
-            self.add_request(request_id, scraper, cursor)
-
-    # TODO: close connection
-    # TODO: add headless option to config
-    # TODO: calnet_user should be passed as argument, not an instance variable
-    # generate a list of argument tuples for each process (unique scrapers and id ranges)
-    def generate_args_add_request_range_parallel(self, start: int, stop: int, process_count=4) -> list[tuple]:
-        process_args = []
-        process_stop = start
-        remaining_tasks = stop - start
-
-        for remaining_processes in range(process_count, 0, -1):
-            scraper = Scraper(user=self.calnet_user, cookies=self.driver_cookies, headless=False)  # pass info from a logged-in browser
-            # prevents the need to log in again for every process
-            cursor = psycopg2.connect(host=self.host, dbname=self.dbname, user=self.user, password=self.password, port=self.port).cursor()
-
-            process_task_count = math.ceil(remaining_tasks / remaining_processes)
-            remaining_tasks -= process_task_count
-            process_start = process_stop
-            process_stop = process_start + process_task_count
-
-            args = (process_start, process_stop, scraper, cursor)
-            process_args.append(args)
-
-        return process_args
-
-    # TODO: implement config for process count
-    # TODO: corner cases for small ranges
-    # scrape and insert a range of work requests into the database (parallel)
-    def add_request_range_parallel(self, start: int, stop: int, process_count=4):
-        process_args = self.generate_args_add_request_range_parallel(start, stop, process_count)
-
-        # run processes in parallel
-        with multiprocessing.Pool(processes=process_count) as pool:
-            pool.starmap(self.add_request_range, process_args)
+            self.add_request(request_id)
 
     # close the connection to the database
     def close(self):
