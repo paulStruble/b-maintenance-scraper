3623384c3176644b1bc7bfc41b1aa76a3297a14b
pjstruble1@gmail.com <pjstruble1@gmail.com>
2024-01-11 03:04:51 -0800

    horizontal lines (menu) - style

--- User.py
+++ User.py
@@ -1,4 +1,5 @@
 # a user with attached login info
+import shutil
 
 from pwinput import pwinput
 
@@ -11,10 +12,12 @@ class User:
     # prompts the user for login credentials and returns a new User object with the same credentials
     @staticmethod
     def login_prompt(hidden: bool = True):
+        print('-' * (shutil.get_terminal_size().columns - 1))
         new_username = input("Username: ")
         if hidden:
             new_password = pwinput(prompt="Password: ")
         else:
             new_password = input("Password: ")
+        print('-' * (shutil.get_terminal_size().columns - 1))
 
         return User(new_username, new_password)
