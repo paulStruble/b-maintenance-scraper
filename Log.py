4e638d0d54404eebe6cfe04e252fc936d87f3cd5
pjstruble1@gmail.com <pjstruble1@gmail.com>
2024-01-13 23:39:15 -0800

    documentation

--- Log.py
+++ Log.py
@@ -6,17 +6,17 @@ from datetime import datetime
 
 class Log:
     def __init__(self):
-        # create and name log
+        """An object linked to a unique .log file that captures events and errors throughout the program."""
         self.filepath = f"./Logs/Log {datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
         with open(self.filepath, 'w') as log:
             log.write(f"log created at time: {datetime.now()}\n\n")
 
-    # add a timestamped message to the log and log in console
     def add(self, message: str):
+        """Add a timestamped message to the log and print it to the console."""
         print(self.add_quiet(message))
 
-    # add a timestamped message to the log (no log in console)
     def add_quiet(self, message: str) -> str:
+        """Add a timestamped message to the log (without printing it to the console)."""
         output = f"[{datetime.now()}] " + message
 
         with open(self.filepath, 'a') as log:
