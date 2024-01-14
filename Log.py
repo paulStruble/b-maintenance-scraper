829f24b31b7441def35df9e176231e9dcc00431d
pjstruble1@gmail.com <pjstruble1@gmail.com>
2024-01-13 23:44:21 -0800

    documentation cleanup

--- Log.py
+++ Log.py
@@ -12,11 +12,19 @@ class Log:
             log.write(f"log created at time: {datetime.now()}\n\n")
 
     def add(self, message: str):
-        """Add a timestamped message to the log and print it to the console."""
+        """Add a timestamped message to the log and print it to the console.
+
+        Args:
+            message: The message to add to the log and printed (excluding timestamp).
+        """
         print(self.add_quiet(message))
 
     def add_quiet(self, message: str) -> str:
-        """Add a timestamped message to the log (without printing it to the console)."""
+        """Add a timestamped message to the log (without printing it to the console).
+
+        Args:
+            message: The message to add to the log (excluding timestamp).
+        """
         output = f"[{datetime.now()}] " + message
 
         with open(self.filepath, 'a') as log:
