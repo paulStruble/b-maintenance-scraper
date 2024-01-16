a64df596911c6275413520c994b6ddf538808ec7
pjstruble1@gmail.com <pjstruble1@gmail.com>
2024-01-15 16:45:20 -0800

    documentation

--- RequestDB.py
+++ RequestDB.py
@@ -1,6 +1,3 @@
-# utilities to communicate with request database
-# TODO: acquire default values from config/profile
-
 import math
 from selenium import webdriver
 import psycopg2
@@ -15,6 +12,7 @@ from typing import Iterable
 class RequestDB:
     def __init__(self, log: Log, calnet_user: User, host: str, dbname: str, user: str, password: str, port: int,
                  process_id: int = 0, headless=True):
+        """A connection to a PostgreSQL database with utilities to add work order request data."""
         self.scraper = Scraper(user=calnet_user, process_id=process_id, headless=headless)
         self.log = log
         self.db_name = dbname
@@ -45,21 +43,20 @@ class RequestDB:
 
         self.connection.commit()
 
-    # scrape and insert a work request into the database
-    def add_request(self, request_id: int):
-        # noinspection PyBroadException
+    def add_request(self, request_id: int) -> None:
+        """Scrape and insert a work request into the database."""
         try:
-            # skip this request if an entry with the same id already exists
+            # Skip this request if an entry with the same id already exists
             select_query = f"SELECT * FROM requests WHERE id = {request_id}"
             self.cursor.execute(select_query)
             if self.cursor.fetchone():
                 self.log.add(f"entry with id [{request_id}] already exists ... skipping this insert request")
                 return None
 
-            # scrape request
+            # Scrape request
             request = self.scraper.scrape_request(request_id)
 
-            # filter out all null values and their corresponding columns
+            # Filter out all null values and their corresponding columns
             columns = [c for c in self.all_columns if getattr(request, c)]
             values = [str(getattr(request, c)).replace("'", "") for c in columns]
             columns = ', '.join(columns)
@@ -73,17 +70,17 @@ class RequestDB:
             self.log.add(f"failed to insert request [{request_id}]")
             self.log.add_quiet(f"{traceback.format_exc()}\n")
 
-    # scraper and insert work requests for an iterable
-    def add_requests(self, request_ids: Iterable[int]):
+    def add_requests(self, request_ids: Iterable[int]) -> None:
+        """Scrape and insert work requests for an iterable of request ids."""
         for request_id in request_ids:
             self.add_request(request_id)
 
-    # scrape and insert a range of work requests into the database
-    def add_request_range(self, start: int, stop: int):
+    def add_request_range(self, start: int, stop: int) -> None:
+        """Scrape and insert a range of work requests into the database."""
         for request_id in range(start, stop):
             self.add_request(request_id)
 
-    # close the connection to the database
-    def close(self):
+    def close(self) -> None:
+        """Close the connection to the database."""
         self.cursor.close()
         self.connection.close()
