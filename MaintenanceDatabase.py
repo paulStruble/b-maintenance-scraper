ad890ea31c39334051600a39d6f3a8b9c0b96a2c
pjstruble1@gmail.com <pjstruble1@gmail.com>
2024-01-17 20:55:31 -0800

    work order implementation

--- MaintenanceDatabase.py
+++ MaintenanceDatabase.py
@@ -30,12 +30,25 @@ class MaintenanceDatabase:
         self.db_name = dbname
         self.db_args = (host, dbname, user, password, port)
 
-        self.all_columns = ["id", "room", "status", "building", "tag", "accept_date", "reject_date", "reject_reason",
-                            "location", "item_description", "work_order_num", "area_description", "requested_action"]
+        self.all_columns_requests = ["id", "room", "status", "building", "tag", "accept_date", "reject_date",
+                                     "reject_reason", "location", "item_description", "work_order_num",
+                                     "area_description", "requested_action"]
+        self.all_columns_orders = ["order_number", "facility", "building", "location_id", "priority", "request_date",
+                                   "schedule_date", "work_status", "date_closed", "main_charge_account", "task_code",
+                                   "reference_number", "tag_number", "item_description", "request_time",
+                                   "date_last_posted", "trade", "contractor_name", "est_completion_date",
+                                   "task_description", "requested_action", "corrective_action"]
 
         self.connection = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
         self.cursor = self.connection.cursor()
 
+        self.initialize_requests_table()
+        self.initialize_orders_table()
+
+        self.connection.commit()
+
+    def initialize_requests_table(self) -> None:
+        """Create database table for work requests if none exists yet."""
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS requests (
             id INT,
             room VARCHAR(20),
@@ -50,10 +63,37 @@ class MaintenanceDatabase:
             work_order_num VARCHAR(25),
             area_description VARCHAR(50),
             requested_action TEXT
-        )
-        """)
+            )
+            """)
 
-        self.connection.commit()
+    # TODO: change "TEXT" to proper types
+    def initialize_orders_table(self) -> None:
+        """Create database table for work orders if none exists yet."""
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS orders (
+            order_number VARCHAR(15),
+            facility VARCHAR(50),
+            building VARCHAR(50),
+            location_id VARCHAR(20),
+            priority TEXT,
+            request_date TEXT,
+            schedule_date TEXT,
+            work_status TEXT,
+            date_closed TEXT,
+            main_charge_account TEXT,
+            task_code TEXT,
+            reference_number TEXT,
+            tag_number TEXT,
+            item_description TEXT,
+            request_time TEXT,
+            date_last_posted TEXT,
+            trade TEXT,
+            contractor_name TEXT,
+            est_completion_date TEXT,
+            task_description TEXT,
+            requested_action TEXT,
+            corrective_action TEXT
+            )
+            """)
 
     def add_request(self, request_id: int) -> None:
         """Scrape and insert a work request into the database.
@@ -73,7 +113,7 @@ class MaintenanceDatabase:
             request = self.scraper.scrape_request(request_id)
 
             # Filter out all null values and their corresponding columns
-            columns = [c for c in self.all_columns if getattr(request, c)]
+            columns = [c for c in self.all_columns_requests if getattr(request, c)]
             values = [str(getattr(request, c)).replace("'", "") for c in columns]
             columns = ', '.join(columns)
             values = ', '.join([f"'{v}'" for v in values])
@@ -104,6 +144,59 @@ class MaintenanceDatabase:
         """
         for request_id in range(start, stop):
             self.add_request(request_id)
+
+    def add_order(self, order_number: str) -> None:
+        """Scrape and insert a work order into the database.
+
+        Args:
+            order_number: Order number of the order.
+        """
+        try:
+            # Skip this request if an entry with the same id already exists
+            select_query = f"SELECT 1 FROM orders WHERE order_number = {order_number}"
+            self.cursor.execute(select_query)
+            if self.cursor.fetchone():
+                self.log.add(f"entry with order number [{order_number}] already exists ... skipping this insert "
+                             f"request")
+                return None
+
+            # Scrape order
+            order = self.scraper.scrape_order(order_number)
+
+            # Filter out all null values and their corresponding columns
+            columns = [c for c in self.all_columns_orders if getattr(order, c)]
+            values = [str(getattr(order, c)).replace("'", "") for c in columns]
+            columns = ', '.join(columns)
+            values = ', '.join([f"'{v}'" for v in values])
+            insert_query = f"INSERT INTO orders ({columns}) VALUES ({values});"
+
+            self.cursor.execute(insert_query)
+            self.connection.commit()
+            self.log.add(f"successfully inserted order [{order_number}] to database [{self.db_name}]")
+        except Exception as e:
+            self.log.add(f"failed to insert order [{order_number}]")
+            self.log.add_quiet(f"{traceback.format_exc()}\n")
+
+    def add_orders(self, order_numbers: Iterable[str]) -> None:
+        """Scrape and insert work orders for an iterable of order numbers.
+
+        Args:
+            order_numbers: Iterable of order numbers.
+        """
+        for order_number in order_numbers:
+            self.add_order(order_number)
+
+    def add_order_range(self, start: int, stop: int, prefix: str = "HM-") -> None:
+        """Scrape and insert a range of work orders into the database.
+
+        Args:
+            start: First order number to scrape and insert (inclusive).
+            stop: Last order number to scrape and insert (exclusive).
+            prefix: String to append the beginning of each integer work order number (default is "HM-").
+        """
+        for int_order_number in range(start, stop):
+            order_number = prefix + str(int_order_number)
+            self.add_order(order_number)
 
     def close(self) -> None:
         """Close the connection to the database."""
